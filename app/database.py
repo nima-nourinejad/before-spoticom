d156890cc547b918fd1a8d9da583381ab3db632a
Nima Nourinejad <nima.nourinejad@gmail.com>
2025-02-07 20:47:00 +0200

    Update database.py

--- app/database.py
+++ app/database.py
@@ -6,3 +6,5 @@ URL_DATABASE = "postgresql://postgres:123456@localhost:5432/game"
 engine = create_engine(URL_DATABASE)
 Session = sessionmaker(auto_commit=False, auto_flush=False, bind=engine)
 Base = declarative_base()
+
+
