f619151481e6c857f2bf2179da9168f4a31d88d4
Nima Nourinejad <nima.nourinejad@gmail.com>
2025-02-11 14:09:59 +0200

    Revert "Revert "Revert "Revert "Revert "Revert "update""""""
    This reverts commit 32152489de828c0ae7cd4317dbd3ef85622bf11d.

--- app/database.py
+++ app/database.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from alembic import context
+from alembic.config import Config
 
 Base = declarative_base()
 
@@ -10,7 +12,7 @@ class Database:
     PASSWORD = "123456"
     DB_NAME = "game"
     HOST_PORT = "localhost:5432"
-    PREFIX = "postgresql+psycopg"
+    PREFIX = "postgresql+psycopg2"
 
     def __init__(self):
         self.__engine = self.__build_engine()
@@ -22,8 +24,8 @@ class Database:
     def __build_engine(self):
         return create_engine(self.__build_url())
 
-    # def __build_tables(self):
-    #     Base.metadata.create_all(bind=self.__engine)
+    def __build_tables(self):
+        Base.metadata.create_all(bind=self.__engine)
 
     def get_session(self):
         session_factory = sessionmaker(
