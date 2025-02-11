009a34411d83315afdecdba1bd5a22cdeccf9561
Nima Nourinejad <nima.nourinejad@gmail.com>
2025-02-11 15:23:23 +0200

    improved database class effiency

--- app/database.py
+++ app/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 Base = declarative_base()
 
@@ -14,6 +14,9 @@ class Database:
 
     def __init__(self):
         self.__engine = self.__build_engine()
+        self.__session_factory = sessionmaker(
+            autocommit=False, autoflush=False, bind=self.__engine
+        )
 
     def __build_url(self):
         return f"{self.PREFIX}://{self.USER}:{self.PASSWORD}@{self.HOST_PORT}/{self.DB_NAME}"
@@ -22,10 +25,7 @@ class Database:
         return create_engine(self.__build_url())
 
     def get_session(self):
-        session_factory = sessionmaker(
-            autocommit=False, autoflush=False, bind=self.__engine
-        )
-        session = session_factory()
+        session: Session = self.__session_factory()
         try:
             yield session
         finally:
