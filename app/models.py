38370afcbc202a55e7c3a5777673bcb5edb67ed1
Nima Nourinejad <nima.nourinejad@gmail.com>
2025-02-21 14:09:14 +0200

    update

--- app/models.py
+++ app/models.py
@@ -2,8 +2,8 @@ from sqlalchemy import Column, Integer, String
 from .database import Base
 
 
-class User(Base):
-    __tablename__ = "user"
+class Users(Base):
+    __tablename__ = "users"
 
     id: Column[int] = Column(Integer, primary_key=True, index=True)
     username: Column[str] = Column(String, index=True)
