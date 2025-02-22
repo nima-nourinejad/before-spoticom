9dca73fbb51755ae0316105fa6135e6c6cff6e23
Nima Nourinejad <nima.nourinejad@gmail.com>
2025-02-22 22:32:10 +0200

    added validation for name

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class SignupRequestSchema(BaseModel):
     name: str
     email: str
     password: str
+
+    @field_validator("name")
+    @classmethod
+    def validate_name(cls, value):
+        if value.strip() == "":
+            raise ValueError("Name cannot be empty")
+        if len(value) > 50:
+            raise ValueError("Name cannot be longer than 50 characters")
+        return value
 
 
 class LoginRequestSchema(BaseModel):
