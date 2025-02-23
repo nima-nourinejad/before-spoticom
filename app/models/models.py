eb4b1b61bf6c6310c109bccaa2b0233da49b3bac
Nima Nourinejad <nima.nourinejad@gmail.com>
2025-02-23 14:05:28 +0200

    update

--- app/models/models.py
+++ app/models/models.py
@@ -7,7 +7,7 @@ from app.database.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id: Column[UUID] = Column(
+    id: Column[uuid.UUID] = Column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
     )
     name: Column[str] = Column(String, index=True)
