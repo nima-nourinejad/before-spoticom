fceaa74523d7102fdf3db0802cb3a09e4eb4afdb
Nima Nourinejad <nima.nourinejad@gmail.com>
2025-02-23 14:15:25 +0200

    used mapped_column instead of column

--- app/models/models.py
+++ app/models/models.py
@@ -1,15 +1,15 @@
 import uuid
 from sqlalchemy import Column, String
 from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy.orm import Mapped, mapped_column
 from app.database.database import Base
-
 
 class User(Base):
     __tablename__ = "users"
 
-    id: Column[uuid.UUID] = Column(
+    id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
     )
-    name: Column[str] = Column(String, index=True)
-    email: Column[str] = Column(String, unique=True, index=True)
-    hashed_password: Column[str] = Column(String, index=True)
+    name: Mapped[str] = mapped_column(String, index=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String, index=True)
